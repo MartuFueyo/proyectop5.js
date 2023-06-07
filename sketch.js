dbeab3c2a98b749aea1c8193bf2a99813f7c8651
Martin Fueyo <martin.fueyo@hotmail.com>
2023-06-07 15:18:54 -0300

    Primera Entrega

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(512, 512);
+}
+
+function draw() {
+    background(220);
+
+    //Circulos Primera fila
+    fill(0);
+    circle(90, 90, 120);
+    circle(250, 90, 120);
+    circle(410, 90, 120);
+
+    //Circulos Segunda fila
+
+    circle(90, 260, 120);
+    circle(250, 260, 120);
+    circle(410, 260, 120);
+
+    //Circulos Tercera fila
+
+    circle(90, 420, 120);
+    circle(250, 420, 120);
+    circle(410, 420, 120);
+}
